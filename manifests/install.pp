14ffbbd3d6a116c32c0335f9934626e990726250
suckatrash <erik.hansen@puppet.com>
2017-08-31 23:28:13 +0000

    add grafana isntallation and update deps

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,4 +17,35 @@ class pe_metrics_dashboard::install {
     ensure  => running,
     require => Package['influxdb'],
   }
+
+  yumrepo { 'grafana-repo':
+    ensure        => 'present',
+    baseurl       => 'https://packagecloud.io/grafana/stable/el/6/$basearch',
+    descr         => 'grafana-repository',
+    enabled       => '1',
+    repo_gpgcheck => '1',
+    gpgcheck      => '1',
+    gpgkey        => 'https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana',
+    sslverify     => '1',
+    sslcacert     => '/etc/pki/tls/certs/ca-bundle.crt',
+  }->
+
+  class { 'grafana':
+    install_method => 'repo',
+    manage_package_repo => false,
+    version => '4.4.3',
+  }
+
+  # Configure grafana to use InfluxDB
+  grafana_datasource { 'influxdb':
+    grafana_url      => 'http://localhost:3000',
+    type             => 'influxdb',
+    url              => 'http://localhost:8086',
+    access_mode      => 'proxy',
+    is_default       => true,
+    grafana_user     => 'admin',
+    grafana_password => 'admin',
+    require          => Service['grafana-server'],
+  }
+
 }
