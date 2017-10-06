369db9f0339be497457ae9c53744fe777bbc11c9
Erik Hansen <erik@iamusingtheinternet.com>
2017-10-06 11:42:46 -0700

    initial work on Ubuntu support

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,21 +1,71 @@
 class pe_metrics_dashboard::install(
   $add_dashboard_examples =   false,
   $influxdb_database_name =   'pe_metrics',
-  $grafana_version =          '4.4.3',
+  $grafana_version =          '4.5.2',
   $grafana_http_port =        '3000',
 ) {
 
-  yumrepo {'influxdb':
-    ensure   => present,
-    enabled  => 1,
-    gpgcheck => 1,
-    baseurl  => 'https://repos.influxdata.com/rhel/$releasever/$basearch/stable',
-    gpgkey   => 'https://repos.influxdata.com/influxdb.key',
+  case $::osfamily {
+
+    'RedHat': {  
+   
+       yumrepo {'influxdb':
+          ensure   => present,
+          enabled  => 1,
+          gpgcheck => 1,
+          baseurl  => 'https://repos.influxdata.com/rhel/$releasever/$basearch/stable',
+          gpgkey   => 'https://repos.influxdata.com/influxdb.key',
+	  before => Package['influxdb'],
+       }
+
+       yumrepo { 'grafana-repo':
+         ensure        => 'present',
+         baseurl       => 'https://packagecloud.io/grafana/stable/el/6/$basearch',
+         descr         => 'grafana-repository',
+         enabled       => '1',
+         repo_gpgcheck => '1',
+         gpgcheck      => '1',
+         gpgkey        => 'https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana',
+         sslverify     => '1',
+         sslcacert     => '/etc/pki/tls/certs/ca-bundle.crt',
+         before        => Class['grafana'],
+       }
+    }
+    
+    'Debian': {
+      
+      $_operatingsystem = downcase($::operatingsystem)	
+
+      apt::source { 'influxdb':
+        location => "https://repos.influxdata.com/${_operatingsystem}",
+        release  => $::lsbdistcodename,
+        repos    => 'stable',
+        key      =>  {
+          'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+          'source' => 'https://repos.influxdata.com/influxdb.key',
+        },
+	before => Package['influxdb'],
+      }
+    
+      apt::source { 'grafana':
+        location => 'https://packagecloud.io/grafana/stable/debian/', 
+        release  => 'jessie',
+        repos    => 'main',
+        key      =>  {
+          'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+          'source' => 'https://packagecloud.io/gpg.key',
+        },
+        before => Package['grafana'],
+      }
+    }
+
+    default: {
+      fail("$::osfamily installation not supported")
+    }
   }
 
   package {'influxdb':
     ensure  => present,
-    require => Yumrepo['influxdb'],
   }
 
   service {'influxdb':
@@ -32,18 +82,6 @@ class pe_metrics_dashboard::install(
     command => "/usr/bin/influx -username admin -password puppet -execute \"create database ${influxdb_database_name}\"",
     unless => "/usr/bin/influx -username admin -password puppet -execute \'show databases\' | grep ${$influxdb_database_name}"
   }
-
-  yumrepo { 'grafana-repo':
-    ensure        => 'present',
-    baseurl       => 'https://packagecloud.io/grafana/stable/el/6/$basearch',
-    descr         => 'grafana-repository',
-    enabled       => '1',
-    repo_gpgcheck => '1',
-    gpgcheck      => '1',
-    gpgkey        => 'https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana',
-    sslverify     => '1',
-    sslcacert     => '/etc/pki/tls/certs/ca-bundle.crt',
-  }->
 
   class { 'grafana':
     install_method => 'repo',
