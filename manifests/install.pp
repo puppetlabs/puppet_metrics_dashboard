f9d3420ed2878b7254ad57f39c2b960374756cfd
suckatrash <erik.hansen@puppet.com>
2017-10-11 20:52:01 +0000

    seperate repos into a new class

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,68 +8,21 @@ class pe_metrics_dashboard::install(
   case $::osfamily {
 
     'RedHat': {  
-       
        $influx_db_service_name = 'influxdb'
-       
-       yumrepo {'influxdb':
-          ensure   => present,
-          enabled  => 1,
-          gpgcheck => 1,
-          baseurl  => 'https://repos.influxdata.com/rhel/$releasever/$basearch/stable',
-          gpgkey   => 'https://repos.influxdata.com/influxdb.key',
-	  before => Package['influxdb'],
-       }
-
-       yumrepo { 'grafana-repo':
-         ensure        => 'present',
-         baseurl       => 'https://packagecloud.io/grafana/stable/el/6/$basearch',
-         descr         => 'grafana-repository',
-         enabled       => '1',
-         repo_gpgcheck => '1',
-         gpgcheck      => '1',
-         gpgkey        => 'https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana',
-         sslverify     => '1',
-         sslcacert     => '/etc/pki/tls/certs/ca-bundle.crt',
-         before        => Class['grafana'],
-       }
     }
-    
     'Debian': {
-     
-      $influx_db_service_name = 'influxd' 
-      $_operatingsystem = downcase($::operatingsystem)	
-
-      apt::source { 'influxdb':
-        location => "https://repos.influxdata.com/${_operatingsystem}",
-        release  => $::lsbdistcodename,
-        repos    => 'stable',
-        key      =>  {
-          'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-          'source' => 'https://repos.influxdata.com/influxdb.key',
-        },
-	before => Package['influxdb'],
-      }
-    
-      apt::source { 'grafana':
-        location => 'https://packagecloud.io/grafana/stable/debian/', 
-        release  => 'jessie',
-        repos    => 'main',
-        key      =>  {
-          'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-          'source' => 'https://packagecloud.io/gpg.key',
-        },
-        before => Package['grafana'],
-      }
+      $influx_db_service_name = 'influxd'
     }
-
     default: {
       fail("$::osfamily installation not supported")
     }
   }
 
+  class {'pe_metrics_dashboard::repos':}->
+
   package {'influxdb':
     ensure  => present,
-  }
+  }->
 
   service {"${influx_db_service_name}":
     ensure  => running,
@@ -138,7 +91,7 @@ class pe_metrics_dashboard::install(
   ## install / enable kapacitor
   package {'kapacitor':
     ensure => present,
-    require => Class['grafana'],
+    require => Class['pe_metrics_dashboard::repos'],
   }->
 
   service {'kapacitor':
@@ -149,7 +102,7 @@ class pe_metrics_dashboard::install(
   ## install / enable telegraf
   package {'telegraf':
     ensure => present,
-    require => Class['grafana'],
+    require => Class['pe_metrics_dashboard::repos'],
   }->  
 
   service {'telegraf':
@@ -161,7 +114,7 @@ class pe_metrics_dashboard::install(
 
   package {'chronograf':
     ensure => present,
-    require => Class['grafana'],
+    require => Class['pe_metrics_dashboard::repos'],
   }->
 
   service {'chronograf':
