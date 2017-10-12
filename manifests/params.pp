c3afd997164f0138f4c7238cd461108fbfd430f9
Jarret Lavallee <jarret.lavallee@puppet.com>
2017-10-12 11:55:00 -0700

    Convert to params.pp format
    Prior to this commit the parameters were in the install.pp class. This
    commit changes the parameters into the params.pp format.
    
    This commit updates to the code to comply with `puppet-lint`.

---
+++ manifests/params.pp
@@ -0,0 +1,31 @@
+# pe_metrics_dashboard::params
+#
+# A description of what this class does
+#
+# @summary Default parameters for the installation
+#
+# @example
+class pe_metrics_dashboard::params {
+
+  # Default Installation parameters
+  $add_dashboard_examples =  false
+  $influxdb_database_name =  'pe_metrics'
+  $grafana_version        =  '4.5.2'
+  $grafana_http_port      =  3000
+  $influx_db_password     =  'puppet'
+  $grafana_password       =  'admin'
+
+  case $::osfamily {
+
+    'RedHat': {
+      $influx_db_service_name = 'influxdb'
+    }
+    'Debian': {
+      $influx_db_service_name = 'influxd'
+    }
+    default: {
+      fail("${::osfamily} installation not supported")
+    }
+  }
+
+}
--- manifests/repos.pp
+++ manifests/repos.pp
@@ -4,27 +4,27 @@ class pe_metrics_dashboard::repos{
 
     'RedHat': {
 
-       yumrepo {'influxdb':
-          ensure   => present,
-          enabled  => 1,
-          gpgcheck => 1,
-          baseurl  => 'https://repos.influxdata.com/rhel/$releasever/$basearch/stable',
-          gpgkey   => 'https://repos.influxdata.com/influxdb.key',
-          before => Package['influxdb'],
-       }
+      yumrepo {'influxdb':
+        ensure   => present,
+        enabled  => 1,
+        gpgcheck => 1,
+        baseurl  => 'https://repos.influxdata.com/rhel/$releasever/$basearch/stable',
+        gpgkey   => 'https://repos.influxdata.com/influxdb.key',
+        before   => Package['influxdb'],
+      }
 
-       yumrepo { 'grafana-repo':
-         ensure        => 'present',
-         baseurl       => 'https://packagecloud.io/grafana/stable/el/6/$basearch',
-         descr         => 'grafana-repository',
-         enabled       => '1',
-         repo_gpgcheck => '1',
-         gpgcheck      => '1',
-         gpgkey        => 'https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana',
-         sslverify     => '1',
-         sslcacert     => '/etc/pki/tls/certs/ca-bundle.crt',
-         before        => Class['grafana'],
-       }
+      yumrepo { 'grafana-repo':
+        ensure        => 'present',
+        baseurl       => 'https://packagecloud.io/grafana/stable/el/6/$basearch',
+        descr         => 'grafana-repository',
+        enabled       => '1',
+        repo_gpgcheck => '1',
+        gpgcheck      => '1',
+        gpgkey        => 'https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana',
+        sslverify     => '1',
+        sslcacert     => '/etc/pki/tls/certs/ca-bundle.crt',
+        before        => Class['grafana'],
+      }
     }
 
     'Debian': {
@@ -39,7 +39,7 @@ class pe_metrics_dashboard::repos{
           'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
           'source' => 'https://repos.influxdata.com/influxdb.key',
         },
-        before => Package['influxdb'],
+        before   => Package['influxdb'],
       }
 
       apt::source { 'grafana':
@@ -50,12 +50,12 @@ class pe_metrics_dashboard::repos{
           'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
           'source' => 'https://packagecloud.io/gpg.key',
         },
-        before => Package['grafana'],
+        before   => Package['grafana'],
       }
     }
 
     default: {
-      fail("$::osfamily installation not supported")
+      fail("${::osfamily} installation not supported")
     }
   }
 
