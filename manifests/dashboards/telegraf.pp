81ff06a3c3f0fc60aa13cfe2b557cb0798bad221
suckatrash <erik.hansen@puppet.com>
2019-04-25 14:21:11 -0700

    last minute fix for issue #47

--- manifests/dashboards/telegraf.pp
+++ manifests/dashboards/telegraf.pp
@@ -9,9 +9,11 @@ class puppet_metrics_dashboard::dashboards::telegraf {
     default => 'http',
   }
 
-  $puppetserver_perf_template = $facts['pe_server_version'] ? {
-    undef   => 'Telegraf_FOSS_Puppetserver_Performance.json',
-    default => 'Telegraf_Puppetserver_Performance.json',
+  if is_function_available('pe_compiling_server_version') {
+    $puppetserver_perf_template = 'Telegraf_Puppetserver_Performance.json'
+  }
+  else {
+    $puppetserver_perf_template = 'Telegraf_FOSS_Puppetserver_Performance.json'
   }
 
   grafana_dashboard {
