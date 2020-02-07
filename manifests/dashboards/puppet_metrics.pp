0693bfab38a337dc2d69bb6dec30563d042c8a16
RandellP <randell@puppet.com>
2020-02-07 11:40:43 -0800

    (SLV-788) New and updated dashboards
    added dashboards for system and process data that was recently added to Puppet metrics collector
    added some new dashboards for ace and bolt data which was recently added to puppet metrics collector
    Added some new dashboards for deeper dives into some of the jvms and puma services
    Added a new orchestration services dashboard for ace, bolt and orch performance data.
    updated spec test with the extra require that was added, and the new dashboards.

--- manifests/dashboards/puppet_metrics.pp
+++ manifests/dashboards/puppet_metrics.pp
@@ -9,12 +9,22 @@ class puppet_metrics_dashboard::dashboards::puppet_metrics {
     default => 'http',
   }
 
+  grafana_folder { 'Deeper Dives':
+    ensure           => present,
+    grafana_url      => "${_uri}://localhost:${puppet_metrics_dashboard::grafana_http_port}",
+    grafana_user     => 'admin',
+    grafana_password => $puppet_metrics_dashboard::grafana_password,
+  }
+
   grafana_dashboard {
     default:
       grafana_url      => "${_uri}://localhost:${puppet_metrics_dashboard::grafana_http_port}",
       grafana_user     => 'admin',
       grafana_password => $puppet_metrics_dashboard::grafana_password,
-      require          => Grafana_datasource['influxdb_puppet_metrics'],
+      require          => [
+        Grafana_datasource['influxdb_puppet_metrics'],
+        Grafana_folder['Deeper Dives'],
+      ],
     ;
     'Archive PuppetDB Performance':
       content => file('puppet_metrics_dashboard/PuppetDB_Performance.json'),
@@ -25,8 +35,34 @@ class puppet_metrics_dashboard::dashboards::puppet_metrics {
     'Archive Puppetserver Performance':
       content => file('puppet_metrics_dashboard/Puppetserver_Performance.json'),
     ;
+    'Archive Orchestration Services':
+      content => file('puppet_metrics_dashboard/Orchestration_Services.json'),
+    ;
+    'Archive Process/System Stats':
+      content => file('puppet_metrics_dashboard/Process_System_Stats.json'),
+    ;
     'Archive File Sync Metrics':
       content => file('puppet_metrics_dashboard/Archive_File_Sync.json'),
+    ;
+    'Archive Ace Puma Performance':
+      content => file('puppet_metrics_dashboard/Ace_Puma_Performance.json'),
+      folder  => 'Deeper Dives',
+    ;
+    'Archive Bolt Puma Performance':
+      content => file('puppet_metrics_dashboard/Bolt_Puma_Performance.json'),
+      folder  => 'Deeper Dives',
+    ;
+    'Archive Orchestrator JVM Performance':
+      content => file('puppet_metrics_dashboard/Orchestrator_JVM_Performance.json'),
+      folder  => 'Deeper Dives',
+    ;
+    'Archive PuppetDB JVM Performance':
+      content => file('puppet_metrics_dashboard/PuppetDB_JVM_Performance.json'),
+      folder  => 'Deeper Dives',
+    ;
+    'Archive Puppetserver JVM Performance':
+      content => file('puppet_metrics_dashboard/Puppetserver_JVM_Performance.json'),
+      folder  => 'Deeper Dives',
     ;
   }
 }
